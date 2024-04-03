12b365e5ca91d1180cdbeadee3f22cebfca3b0e0
yk132 <53902254+yk132@users.noreply.github.com>
2024-04-03 16:07:50 -0400

    updated to work directory

--- Job2_merge_pod5.sh
+++ Job2_merge_pod5.sh
@@ -9,7 +9,9 @@ export FILE_DIR="${CAFO_DIR}/20230908_1516_MN33275_FAK80584_b5659fa0"
 export SKIP_DIR="${FILE_DIR}/pod5_skip"
 export PASS_DIR="${FILE_DIR}/pod5_pass"
 export FAIL_DIR="${FILE_DIR}/pod5_fail"
-export ALL_DIR="${FILE_DIR}/pod5_all"
+## output in work folder
+export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
+export ALL_DIR="${WORK_DIR}/pod5_all"
 export BARCODE01_DIR="${ALL_DIR}/barcode01"
 export BARCODE02_DIR="${ALL_DIR}/barcode02"
 export BARCODE03_DIR="${ALL_DIR}/barcode03"
@@ -24,7 +26,7 @@ mkdir -p $UNCLASSIFIED_DIR
 
 cp $SKIP_DIR/* $UNCLASSIFIED_DIR
 
-cp -n $PASS_DIR/barcode01/* $BARCODE01_DIR
+cp -n $PASS_DIR/barcode01/* $BARCODE01_DIR # -n means don't overwrite existing file
 cp -n $PASS_DIR/barcode02/* $BARCODE02_DIR
 cp -n $PASS_DIR/barcode03/* $BARCODE03_DIR
 cp -n $PASS_DIR/unclassified/* $UNCLASSIFIED_DIR
