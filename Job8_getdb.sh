116d9de2e405143b45d6d490341ac59228329624
Eva Kim <yk132@duke.edu>
2024-04-19 13:19:59 -0400

    realize path to fastq file was wrong

--- Job8_getdb.sh
+++ Job8_getdb.sh
@@ -18,17 +18,18 @@ export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
 export NXF_WORK="${CAFO_WORK_DIR}/nf_working"
 export NXF_TEMP="${CAFO_WORK_DIR}/nf_temp"
 export DATA_DIR="${CAFO_DIR}/20230908_1516_MN33275_FAK80584_b5659fa0"
-export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
-export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
+#export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
+#export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
 export OUTPUT_DIR="${CAFO_DIR}/Outputs"
-export DB_DIR="${$OUTPUT_DIR}/Job8_db"
+export MERGED_DIR="${OUTPUT_DIR}/Job4_merged_fastq"
+export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
+export DB_DIR="${OUTPUT_DIR}/Job8_db"
 export RESULTS_DIR="${OUTPUT_DIR}/Job8_wf_meta_barcode03_res"
 export SLURM_CPUS_PER_TASK="30" # CHANGE ME
 #-------------------------------
 
 # make directories
 
-mkdir -p $SCRATCH_DIR
 mkdir -p $DB_DIR
 mkdir -p $NXF_WORK
 mkdir -p $NXF_TEMP
--- main_job5_7_contigs.sh
+++ main_job5_7_contigs.sh
@@ -14,16 +14,16 @@ mkdir -p ${LOG_DIR}
 # this file covers: building contigs with metaSPAdes, QC with QUAST, mapping with Bowtie2, and visualization with angi'o  
 
 # Download Dorado Models
-# JOBID_5=$(sbatch --parsable --job-name=metaspades --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job5_metaSPAdes.sh)
+JOBID_5=$(sbatch --parsable --job-name=metaspades --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job5_metaSPAdes.sh)
 
 # quailty check with QUAST
-# JOBID_6=$(sbatch --parsable --dependency=afterok:${JOBID_5} --job-name=quast --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job6_QUAST.sh)
+JOBID_6=$(sbatch --parsable --dependency=afterok:${JOBID_5} --job-name=quast --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job6_QUAST.sh)
 
 # Mapping: for now, build bowtie2 index
-# JOBID_7=$(sbatch --parsable --dependency=afterok:${JOBID_5}:${JOBID_6} --job-name=mapping --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job7_mapping.sh)
+JOBID_7=$(sbatch --parsable --dependency=afterok:${JOBID_5}:${JOBID_6} --job-name=mapping --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job7_mapping.sh)
 
 ## This is for CHECKING new code only! Dependency has been removed. 
 #JOBID_6=$(sbatch --parsable --job-name=quast --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job6_QUAST.sh)
 
 ## This is for CHECKING new code only! Dependency has been removed. 
-JOBID_7=$(sbatch --parsable --job-name=bwa --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job7_bwa.sh)
+# JOBID_7=$(sbatch --parsable --job-name=bwa --output="$LOG_DIR/%x.%j.out" --error="$LOG_DIR/%x.%j.err" Job7_bwa.sh)
