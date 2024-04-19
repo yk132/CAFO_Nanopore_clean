b01e0fc0c7add4e09e482e338df4dfa196c3936e
Eva Kim <yk132@duke.edu>
2024-04-19 11:27:35 -0400

    cleared up file directories

--- Job8_getdb.sh
+++ Job8_getdb.sh
@@ -7,14 +7,14 @@
 export STORE_DIR="/hpc/group/gunschlab/yk132/storage"
 export SCRATCH_DIR="/work/yk132/CAFO_Nanopore"
 export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
-export DB_DIR="${STORE_DIR}/wf_meta_db/ncbi_16s_18s_28s_ITS_db"
 export NXF_WORK="${SCRATCH_DIR}/nf_working"
 export NXF_TEMP="${SCRATCH_DIR}/nf_temp"
 export DATA_DIR="${STORE_DIR}/20230908_Dust_Redo_R9/20230908_1516_MN33275_FAK80584_b5659fa0"
 export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
 export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
 export OUTPUT_DIR="${CAFO_DIR}/Outputs"
-export RESULTS_DIR="${OUTPUT_DIR}/wf_meta_barcode03_res"
+export DB_DIR="${$OUTPUT_DIR}/Job8_db"
+export RESULTS_DIR="${OUTPUT_DIR}/Job8_wf_meta_barcode03_res"
 #-------------------------------
 
 # make directories
