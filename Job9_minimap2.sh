d17b03e04a1d716c19df8b506b16d8a2927eb15b
yk132 <53902254+yk132@users.noreply.github.com>
2024-04-19 11:33:44 -0400

    start cleaning up directories

--- Job9_minimap2.sh
+++ Job9_minimap2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --partition=dmcshared
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=300G
 #SBATCH --mail-type=ALL
 
@@ -9,6 +9,42 @@ set -u
 
 #-------------------------------
 export STORE_DIR="/hpc/home/yk132/storage"
+export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
+export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore_clean"
+export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
+export CAFO_WORK_DIR="${WORK_DIR}/CAFO_Nanopore_work"
+export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
+export NXF_WORK="${CAFO_WORK_DIR}/nf_working"
+export NXF_TEMP="${CAFO_WORK_DIR}/nf_temp"
+export DATA_DIR="${CAFO_DIR}/20230908_1516_MN33275_FAK80584_b5659fa0"
+export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
+export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
+export OUTPUT_DIR="${CAFO_DIR}/Outputs"
+export DB_DIR="${$OUTPUT_DIR}/Job8_db"
+export RESULTS_DIR="${OUTPUT_DIR}/Job8_wf_meta_barcode03_res"
+export SLURM_CPUS_PER_TASK="30" # CHANGE ME
+
+# MERGE!!!
+
+export STORE_DIR="/hpc/group/gunschlab/yk132/storage"
+export SCRATCH_DIR="/work/yk132/CAFO_Nanopore"
+export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
+export NXF_WORK="${SCRATCH_DIR}/nf_working"
+export NXF_TEMP="${SCRATCH_DIR}/nf_temp"
+export DATA_DIR="${STORE_DIR}/20230908_Dust_Redo_R9/20230908_1516_MN33275_FAK80584_b5659fa0"
+export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
+export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
+export OUTPUT_DIR="${CAFO_DIR}/Outputs"
+export DB_DIR="${$OUTPUT_DIR}/Job8_db"
+
+export STORE_DIR="/hpc/home/yk132/storage"
+export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
+export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore_clean"
+export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
+export CAFO_WORK_DIR="${WORK_DIR}/CAFO_Nanopore_work"
+export OUTPUT_DIR="${CAFO_DIR}/Outputs"
+export SLURM_CPUS_PER_TASK="32" # CHANGE ME
+
 export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
 export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore"
 export WORK_DIR="/work/${USER}"
