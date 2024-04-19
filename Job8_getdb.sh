aefa26393a6ddea4060c3c82970dcf06b7da6a97
yk132 <53902254+yk132@users.noreply.github.com>
2024-04-19 11:33:17 -0400

    clean up directories and fix num threads

--- Job8_getdb.sh
+++ Job8_getdb.sh
@@ -9,17 +9,21 @@
 # using pipeline ONLY to download the RefSeq marker gene database formatted for MiniMap2
 
 #-------------------------------
-export STORE_DIR="/hpc/group/gunschlab/yk132/storage"
-export SCRATCH_DIR="/work/yk132/CAFO_Nanopore"
+export STORE_DIR="/hpc/home/yk132/storage"
+export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
+export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore_clean"
+export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
+export CAFO_WORK_DIR="${WORK_DIR}/CAFO_Nanopore_work"
 export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
-export NXF_WORK="${SCRATCH_DIR}/nf_working"
-export NXF_TEMP="${SCRATCH_DIR}/nf_temp"
-export DATA_DIR="${STORE_DIR}/20230908_Dust_Redo_R9/20230908_1516_MN33275_FAK80584_b5659fa0"
+export NXF_WORK="${CAFO_WORK_DIR}/nf_working"
+export NXF_TEMP="${CAFO_WORK_DIR}/nf_temp"
+export DATA_DIR="${CAFO_DIR}/20230908_1516_MN33275_FAK80584_b5659fa0"
 export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
 export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
 export OUTPUT_DIR="${CAFO_DIR}/Outputs"
 export DB_DIR="${$OUTPUT_DIR}/Job8_db"
 export RESULTS_DIR="${OUTPUT_DIR}/Job8_wf_meta_barcode03_res"
+export SLURM_CPUS_PER_TASK="30" # CHANGE ME
 #-------------------------------
 
 # make directories
@@ -38,7 +42,7 @@ nextflow run epi2me-labs/wf-metagenomics/main.nf \
 	--fastq $BARCODE03_FASTQ \
 	--database_set ncbi_16s_18s_28s_ITS \
 	--classifier minimap2 \
-	--threads 40 \
+	--threads $SLURM_CPUS_PER_TASK \
 	--out_dir $RESULTS_DIR \
 	--min_len 50 \
 	--store_dir $DB_DIR 
