ab9d83d38815fd76c1f3c82c85e3dcc76c1a60d3
yk132 <53902254+yk132@users.noreply.github.com>
2024-04-19 12:44:44 -0400

    cleaned up directories and code

--- Job10_bam_to_tsv.sh
+++ Job10_bam_to_tsv.sh
@@ -12,25 +12,28 @@ set -u
 #-------------------------------
 export STORE_DIR="/hpc/home/yk132/storage"
 export CAFO_DIR="${STORE_DIR}/20230908_Dust_Redo_R9"
-export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore"
+export CAFO_GIT_DIR="${CAFO_DIR}/CAFO_Nanopore_clean"
+export WORK_DIR="/work/${USER}" # CHANGE ME if needed 
+export CAFO_WORK_DIR="${WORK_DIR}/CAFO_Nanopore_work"
+export IMAGEDIR="${WORK_DIR}/images"
+export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
+export NXF_WORK="${CAFO_WORK_DIR}/nf_working"
+export NXF_TEMP="${CAFO_WORK_DIR}/nf_temp"
+export DATA_DIR="${CAFO_DIR}/20230908_1516_MN33275_FAK80584_b5659fa0"
+export MERGED_DIR="${DATA_DIR}/dorado_fastq_results/Merged_bam_fastq"
+export BARCODE03_FASTQ="${MERGED_DIR}/barcode03_merged.fastq"
+export OUTPUT_DIR="${CAFO_DIR}/Outputs"
+export DB_DIR="${$OUTPUT_DIR}/Job8_db"
 export SLURM_CPUS_PER_TASK="40" # CHANGE ME
-export MINIMAP2_OUTPUT="${CAFO_DIR}/Minimap_res"
-export DATABASE_PARENT_DIR="${STORE_DIR}/wf_meta_db/ncbi_16s_18s_28s_ITS"
-export DATABASE_DIR="${DATABASE_PARENT_DIR}/ncbi_16s_18s_28s_ITS_db"
-export DATABASE_FNA="${DATABASE_DIR}/ncbi_16s_18s_28s_ITS.fna"
-export DATABASE_MMI="${DATABASE_DIR}/ncbi_16s_18s_28s_ITS.mmi"
+export MERGED_DIR="${DORADO_RESULTS_DIR}/Merged_bam_fastq"
+export MINIMAP2_OUTPUT="${OUTPUT_DIR}/Job9_Minimap"
 export BAR01_BAM="${MINIMAP2_OUTPUT}/barcode01_alignment.bam"
 export BAR02_BAM="${MINIMAP2_OUTPUT}/barcode02_alignment.bam"
 export BAR03_BAM="${MINIMAP2_OUTPUT}/barcode03_alignment.bam"
-
-export NXF_SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
-export DB_DIR="${STORE_DIR}/wf_meta_db/ncbi_16s_18s_28s_ITS_db"
-export SCRATCH_DIR="/work/yk132/CAFO_Nanopore"
-export NXF_WORK="${SCRATCH_DIR}/nf_working"
-export NXF_TEMP="${SCRATCH_DIR}/nf_temp"
-export RES_DIR01="${CAFO_DIR}/wf_meta_bam_res_barcode01"
-export RES_DIR02="${CAFO_DIR}/wf_meta_bam_res_barcode02"
-export RES_DIR03="${CAFO_DIR}/wf_meta_bam_res_barcode03"
+export RES_DIR="${TSV_OUTDIR}/Job10_bam_to_tsv"
+export RES_DIR01="${TSV_OUTDIR}/res_barcode01"
+export RES_DIR02="${TSV_OUTDIR}/res_barcode02"
+export RES_DIR03="${TSV_OUTDIR}/res_barcode03"
 #------------------------------
 
 # make directories
@@ -47,16 +50,37 @@ mkdir -p $RES_DIR03
 nextflow pull epi2me-labs/wf-metagenomics/
 
 # Run nextflow on barcode 01
+nextflow run epi2me-labs/wf-metagenomics/main.nf \
+	-profile singularity \
+	-c ./nextflow.config \
+	--bam $BAR01_BAM \
+	--database_set ncbi_16s_18s_28s_ITS \
+	--classifier minimap2 \
+	--threads $SLURM_CPUS_PER_TASK \
+	--out_dir $RES_DIR01 \
+	--min_len 50 \
+	--store_dir $DB_DIR 
 
+# barcode02
 nextflow run epi2me-labs/wf-metagenomics/main.nf \
 	-profile singularity \
 	-c ./nextflow.config \
 	--bam $BAR02_BAM \
 	--database_set ncbi_16s_18s_28s_ITS \
 	--classifier minimap2 \
-	--threads 40 \
+	--threads $SLURM_CPUS_PER_TASK \
 	--out_dir $RES_DIR02 \
 	--min_len 50 \
-#	--amr \
-#	--amr_db resfinder \
+	--store_dir $DB_DIR 
+ 
+# barcode03
+nextflow run epi2me-labs/wf-metagenomics/main.nf \
+	-profile singularity \
+	-c ./nextflow.config \
+	--bam $BAR03_BAM \
+	--database_set ncbi_16s_18s_28s_ITS \
+	--classifier minimap2 \
+	--threads $SLURM_CPUS_PER_TASK \
+	--out_dir $RES_DIR03 \
+	--min_len 50 \
 	--store_dir $DB_DIR 
