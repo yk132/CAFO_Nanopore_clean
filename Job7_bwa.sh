5c97b4bc326b3858f8826250bd6bca396de9d5a1
yk132 <53902254+yk132@users.noreply.github.com>
2024-04-18 22:28:43 -0400

    finished changing order

--- Job7_bwa.sh
+++ Job7_bwa.sh
@@ -26,32 +26,22 @@ export SPADES_2="${SPADES_OUTPUT}/barcode02"
 export CONTIG_01="${SPADES_1}/contigs.fasta"
 export CONTIG_02="${SPADES_2}/contigs.fasta"
 export MAP_DIR="${OUTPUT_DIR}/Job7_mapping_bwa"
-
 #-------------------------------
 
 #-------------------------------
 mkdir -p $MAP_DIR
 #-------------------------------
 
-# Build index
-## barcode 1 is Farm C 
+# Barcode 1 is Farm C
+## build index
 cd $SPADES_1
 singularity exec \
 	--bind /work:/work \
 	--bind /hpc/group:/hpc/group \
         docker://staphb/bwa:0.7.17 \
         bwa index $CONTIG_01 -p barcode01
-	
-## barcode 2 is Farm A 
-cd $SPADES_2
-singularity exec \
-	--bind /work:/work \
-	--bind /hpc/group:/hpc/group \
-        docker://staphb/bwa:0.7.17 \
-        bwa index $CONTIG_02 -p barcode02
 
-# Run bwa mem on Nanopore long reads: NOT paired end!!
-cd $SPADES_1
+## Run bwa mem on Nanopore long reads: NOT paired end!!
 singularity exec \
 	--bind /work:/work \
 	--bind /hpc/group:/hpc/group \
@@ -59,43 +49,39 @@ singularity exec \
         bwa mem barcode01 -t $SLURM_CPUS_PER_TASK \
 	$BAR01_MERGED > $MAP_DIR/barcode01_nanopore.sam
 
+## Illumina short reads
 singularity exec \
 	--bind /work:/work \
 	--bind /hpc/group:/hpc/group \
         docker://staphb/bwa:0.7.17 \
-        bwa mem $CONTIG_02 -t $SLURM_CPUS_PER_TASK \
-	$BAR02_MERGED > $MAP_DIR/barcode02_nanopore.sam
-
-# Run bwa mem on Illumina short reads
-## barcode 1 is Farm C
-singularity exec \
-	--bind /work:/work \
-	--bind /hpc/group:/hpc/group \
-       docker://staphb/bwa:0.7.17 \
-        bwa mem $CONTIG_01 -t $SLURM_CPUS_PER_TASK \
+        bwa mem barcode01 -t $SLURM_CPUS_PER_TASK \
 	$ILAB_DIR/Farm_C_Dust_S2_L001_R1_001.fastq.gz \
  	$ILAB_DIR/Farm_C_Dust_S2_L001_R2_001.fastq.gz  > $MAP_DIR/barcode01_illumina_pe.sam
 
-## barcode 2 is Farm A
+
+ 
+# barcode 2 is Farm A 
+## build index
+cd $SPADES_2
 singularity exec \
 	--bind /work:/work \
 	--bind /hpc/group:/hpc/group \
         docker://staphb/bwa:0.7.17 \
-        bwa mem $CONTIG_02 -t $SLURM_CPUS_PER_TASK \
+        bwa index $CONTIG_02 -p barcode02
+
+## map Nanopore reads
+singularity exec \
+	--bind /work:/work \
+	--bind /hpc/group:/hpc/group \
+        docker://staphb/bwa:0.7.17 \
+        bwa mem barcode02 -t $SLURM_CPUS_PER_TASK \
 	$BAR02_MERGED > $MAP_DIR/barcode02_nanopore.sam
 
+## map Illumina reads
 singularity exec \
-	--bind /work:/work \
-	--bind /work:/work \
 	--bind /work:/work \
 	--bind /hpc/group:/hpc/group \
         docker://staphb/bwa:0.7.17 \
-        bwa mem $CONTIG_02 -t $SLURM_CPUS_PER_TASK \
+        bwa mem barcode02 -t $SLURM_CPUS_PER_TASK \
 	$ILAB_DIR/Farm_A_Dust_S3_L001_R1_001.fastq.gz 
  	$ILAB_DIR/Farm_A_Dust_S3_L001_R2_001.fastq.gz  > $MAP_DIR/barcode02_illumina_pe.sam
-
-singularity exec \
-	--bind /work:/work \
-	--bind /hpc/group:/hpc/group \
-        docker://staphb/bwa:0.7.17 \
-        bwa mem $CONTIG_02 -t $SLURM_CPUS_PER_TASK \
